97e8ed611bced9f0e3b5887c88888f3c8dd52489
ahmedmohd957 <ahmed.mohd957@gmail.com>
2024-05-20 23:19:03 +0200

    fix: :bug: Fixes bug with navbar

--- src/layouts/pageLayout.jsx
+++ src/layouts/pageLayout.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import Navbar from '../components/navbar/navbar'
 import Footer from '../components/footer/footer'
-import Example from '../components/navbar/navbar1'
 
 const PageLayout = ({ children }) => {
   return (
     <>
-      {/* <Navbar /> */}
-      <Example />
+      <Navbar />
       <div className='bg-background min-h-screen overflow-hidden'>
         <div className='container mx-auto flex-grow p-2 md:p-4 overflow-hidden'>
           {children}
