ea1f38ee4714c204fba56eb5b6d5f97ed6bcc299
ahmedmohd957 <ahmed.mohd957@gmail.com>
2024-05-21 15:24:06 +0200

    refactor: :recycle: Refactor code for user profile

--- src/components/profile/postsGrid.jsx
+++ src/components/profile/postsGrid.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Post from '../post/post'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { fetchUserPosts } from '../../store/actions/postActions'
 import ProfilePost from '../post/profilePost'
 import Loading from '../../components/loading'
@@ -8,18 +8,26 @@ import { getPosts } from '../../api/post'
 
 const PostsGrid = ({ user, currentUser, isCurrentUser }) => {
   const dispatch = useDispatch()
-  const [posts, setPosts] = useState([])
+  const currentUserPosts = useSelector(state => state.post.data)
+  const [posts, setPosts] = useState(null)
 
   useEffect(() => {
-    dispatch(fetchUserPosts(user._id))
-    const fetchPosts = async () => {
-      const data = await getPosts(user._id)
-      setPosts(data)
+    if (isCurrentUser) {
+      dispatch(fetchUserPosts(user._id))
+    } else {
+      const fetchPosts = async () => {
+        const data = await getPosts(user._id)
+        setPosts(data)
+      }
+      fetchPosts()
     }
-    fetchPosts()
-  }, [dispatch, user._id])
+  }, [dispatch, isCurrentUser, user._id])
 
-  if (!posts) {
+  if (isCurrentUser && !currentUserPosts) {
+    return <Loading />
+  }
+
+  if (!isCurrentUser && !posts) {
     return <Loading />
   }
 
@@ -27,13 +35,17 @@ const PostsGrid = ({ user, currentUser, isCurrentUser }) => {
     <div className='py-2 md:p-4 my-4'>
       <h2 className='text-xl sm:text-2xl font-bold mb-4'>Posts</h2>
       <div className='grid grid-cols-1 gap-0 sm:gap-4'>
-        {posts.map(post =>
-          isCurrentUser ? (
-            <Post key={post._id} post={post} currentUser={currentUser} />
-          ) : (
-            <ProfilePost key={post._id} post={post} currentUser={currentUser} />
-          )
-        )}
+        {isCurrentUser
+          ? currentUserPosts.map(post => (
+              <Post key={post._id} post={post} currentUser={currentUser} />
+            ))
+          : posts.map(post => (
+              <ProfilePost
+                key={post._id}
+                post={post}
+                currentUser={currentUser}
+              />
+            ))}
       </div>
     </div>
   )
